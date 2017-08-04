bcac1c1d6db3e79305ba6bd4943b25e4bdcb59a3
= <mbfrahry@gmail.com>
2017-08-04 00:25:06 -0600

    readd test config

--- opc/resource_database_service_instance_test.go
+++ opc/resource_database_service_instance_test.go
@@ -117,3 +117,27 @@ func testAccDatabaseServiceInstanceBasic(rInt int) string {
     }
   }`, rInt)
 }
+
+func testAccDatabaseServiceInstanceCloudStorage(rInt int) string {
+	return fmt.Sprintf(`resource "opc_database_service_instance" "test" {
+    name        = "test-service-instance-%d"
+    description = "test service instance"
+    edition = "EE_EP"
+    level = "PAAS"
+    shape = "oc1m"
+    subscription_type = "HOURLY"
+    version = "12.2.0.1"
+    vm_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC3QxPp0BFK+ligB9m1FBcFELyvN5EdNUoSwTCe4Zv2b51OIO6wGM/dvTr/yj2ltNA/Vzl9tqf9AUBL8tKjAOk8uukip6G7rfigby+MvoJ9A8N0AC2te3TI+XCfB5Ty2M2OmKJjPOPCd6+OdzhT4cWnPOM+OAiX0DP7WCkO4Kx2kntf8YeTEurTCspOrRjGdo+zZkJxEydMt31asu9zYOTLmZPwLCkhel8vY6SnZhDTNSNkRzxZFv+Mh2VGmqu4SSxfVXr4tcFM6/MbAXlkA8jo+vHpy5sC79T4uNaPu2D8Ed7uC3yDdO3KRVdzZCfWHj4NjixdMs2CtK6EmyeVOPuiYb8/mcTybrb4F/CqA4jydAU6Ok0j0bIqftLyxNgfS31hR1Y3/GNPzly4+uUIgZqmsuVFh5h0L7qc1jMv7wRHphogo5snIp45t9jWNj8uDGzQgWvgbFP5wR7Nt6eS0kaCeGQbxWBDYfjQE801IrwhgMfmdmGw7FFveCH0tFcPm6td/8kMSyg/OewczZN3T62ETQYVsExOxEQl2t4SZ/yqklg+D9oGM+ILTmBRzIQ2m/xMmsbowiTXymjgVmvrWuc638X6dU2fKJ7As4hxs3rA1BA5sOt0XyqfHQhtYrL/Ovb1iV+C7MRhKicTyoNTc7oVcDDG0VW785d8CPqttDi50w=="
+    parameter {
+      admin_password = "Test_String7"
+      backup_destination = "OSS"
+      failover_database = false
+      sid = "ORCL"
+      usable_storage = 15
+    }
+    cloud_storage {
+      container = "Storage-canonical/matthew-test-%d"
+      create_if_missing = true
+    }
+	}`, rInt, rInt)
+}
